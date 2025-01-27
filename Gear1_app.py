2c1e1164de1b0b5e3c8820b15eb8dfb39c1b8db9
Vitor Petta Batista <vitor_pbatista@outlook.com>
2025-01-27 17:30:34 -0300

    fixing mergin errors

--- Gear1_app.py
+++ Gear1_app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -19,26 +18,4 @@ st.set_page_config(
 st.image("https://gear1.gg/wp-content/uploads/2022/11/Cabecalho.png")
 st.title(":green[Bem-vindo ao Gear One Head Quarter]")
 
-=======
-import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-
-st.set_page_config(
-    page_title="Gear 1 HQ",
-    page_icon="https://gear1.gg/wp-content/uploads/2022/11/Cabecalho.png",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    # menu_items={
-    #     'Get Help': 'https://www.extremelycoolapp.com/help',
-    #     'Report a bug': "https://www.extremelycoolapp.com/bug",
-    #     'About': "# This is a header. This is an *extremely* cool app!"
-    # }
-)
-st.image("https://gear1.gg/wp-content/uploads/2022/11/Cabecalho.png")
-st.title(":green[Bem-vindo ao Gear One Head Quarter]")
-
->>>>>>> 3d19a0024fdcf605e96fcda6fa04c48cd4243892
 st.write(":green[Utilize o menu à esquerda para navegar entre as páginas.]")
