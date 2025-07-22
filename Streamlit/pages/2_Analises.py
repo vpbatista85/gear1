92c5e56a9145ad117fd4e156b7dae909b8dfa83f
Vitor Petta Batista <vitor_pbatista@outlook.com>
2025-07-22 16:16:40 -0300

    fixing functions at analises page

--- Streamlit/pages/2_Analises.py
+++ Streamlit/pages/2_Analises.py
@@ -455,44 +455,236 @@ def main():
         for info in info_arquivos:
             st.text(info)
 
-        st.subheader("Temperatura ao longo do tempo")
-        col1, col2 = st.columns(2)
-        if 'Track Temp' in df_filtrado.columns:
-            fig_track_temp = px.line(df_filtrado, x='Started at', y='Track Temp', title='Temperatura da Pista')
-            col1.plotly_chart(fig_track_temp, use_container_width=True)
-        if 'Air Temp' in df_filtrado.columns:
-            fig_air_temp = px.line(df_filtrado, x='Started at', y='Air Temp', title='Temperatura do Ar')
-            col2.plotly_chart(fig_air_temp, use_container_width=True)
+        tab1, tab2, tab3, tab4 = st.tabs([":green[Lap Time]", ":green[Safety]", ":green[Fuel]", ":green[Temperature]"])
+        # Define color sets of paintings
+        gear1_colors = ['rgb(25, 128, 37)','rgb(255, 127, 0)']#verde, laranja
 
-        if 'Fuel Used' in df_filtrado.columns:
-            st.subheader("Histograma do Consumo de Combustível por Volta")
-            fig_fuel = px.histogram(df_filtrado, x='Fuel Used', nbins=30, title='Consumo por Volta (Fuel Used)')
-            st.plotly_chart(fig_fuel, use_container_width=True)
+        with tab1:
+            
+            bin_width = st.sidebar.slider(
+                ":green[Tamanho do Intervalo (segundos)]",
+                min_value=0.250,
+                max_value=1.000,
+                value=0.500,
+                step=0.250
+            )
 
-        if 'Driver' in df_filtrado.columns and 'Clean' in df_filtrado.columns:
-            st.subheader("Voltas Limpas vs Incidentes por Piloto")
-            grouped = df_filtrado.groupby(['Driver', 'Clean']).size().reset_index(name='Counts')
-            voltas_limpas = grouped[grouped['Clean'] == 1]
-            voltas_com_incidentes = grouped[grouped['Clean'] == 0]
+            lap_times_in_seconds = final_df["Lap time"].dt.total_seconds()
+            Q1 = lap_times_in_seconds.quantile(0.25)
+            Q3 = lap_times_in_seconds.quantile(0.75)
+            IQR = Q3 - Q1
+            filtered_df = final_df[
+                (lap_times_in_seconds >= Q1 - 1.5 * IQR) & (lap_times_in_seconds <= Q3 + 1.5 * IQR)
+            ]
 
-            fig = go.Figure()
-            fig.add_trace(go.Bar(x=voltas_limpas['Driver'], y=voltas_limpas['Counts'], name='Voltas Limpas', marker_color='rgb(25, 128, 37)'))
-            fig.add_trace(go.Bar(x=voltas_com_incidentes['Driver'], y=voltas_com_incidentes['Counts'], name='Voltas com Incidente', marker_color='rgb(255, 127, 0)'))
-            fig.update_layout(title='Total de Voltas por Piloto', xaxis_title='Piloto', yaxis_title='Número de Voltas', barmode='stack', template='plotly_white')
-            st.plotly_chart(fig)
+            min_lap_time = filtered_df["Lap time"].dt.total_seconds().min()
+            max_lap_time = filtered_df["Lap time"].dt.total_seconds().max()
+            num_bins = int(np.ceil((max_lap_time - min_lap_time) / bin_width))
 
-            sequencias_df = calcular_sequencias_voltas_limpas(df_filtrado)
+            fig_all = go.Figure()
+            fig_all.add_trace(go.Histogram(
+                x=filtered_df["Lap time"].dt.total_seconds(),
+                nbinsx=num_bins,
+                marker_color=gear1_colors[0],
+                opacity=0.75
+            ))
+
+            tick_vals = np.arange(min_lap_time, max_lap_time + bin_width, bin_width)
+            tick_texts = [f"{int(t // 60):02}:{int(t % 60):02}.{int((t * 1000) % 1000):03}" for t in tick_vals]
+
+            fig_all.update_layout(
+                title="Histograma Geral para Todos os Pilotos",
+                xaxis_title="Tempo de Volta (MM:SS.mmm)",
+                yaxis_title="Frequência (Nº Voltas)",
+                xaxis=dict(
+                    tickmode='array',
+                    tickvals=tick_vals,
+                    ticktext=tick_texts,
+                    range=[min_lap_time, max_lap_time]
+                )
+            )
+
+            fig_all.update_layout(bargap=0.02)#test
+            st.plotly_chart(fig_all, use_container_width=True)
+
+            drivers = filtered_df["Driver"].unique()
+
+            for driver in drivers:
+                driver_data = filtered_df[filtered_df["Driver"] == driver]
+
+                fig = make_subplots(rows=1, cols=2, shared_yaxes=True, column_widths=[0.7, 0.3])
+
+                fig.add_trace(go.Histogram(
+                    x=driver_data["Lap time"].dt.total_seconds(),
+                    nbinsx=num_bins,
+                    marker_color=gear1_colors[1],
+                    opacity=0.75,
+                    name="Histograma"
+                ), row=1, col=1)
+
+                fig.add_trace(go.Box(
+                    x=driver_data["Lap time"].dt.total_seconds(),
+                    marker_color=gear1_colors[1],
+                    name="Box Plot",
+                    # boxpoints='all',
+                    # jitter=0.3,
+                    # pointpos=-1.8,
+                ), row=1, col=2)
+
+                fig.update_layout(
+                    title=f"Análise para {driver}",
+                    xaxis_title="Tempo de Volta (MM:SS.mmm)",
+                    yaxis_title="Frequência (Nº Voltas)",
+                    xaxis=dict(
+                        tickmode='array',
+                        tickvals=tick_vals,
+                        ticktext=tick_texts,
+                        range=[min_lap_time, max_lap_time]
+                    ),
+                    xaxis2=dict(
+                        tickmode='array',
+                        tickvals=tick_vals,
+                        ticktext=tick_texts,
+                        range=[min_lap_time, max_lap_time]
+                    ),
+                    showlegend=False
+                )
+                fig.update_xaxes(tickangle=45)
+                fig.update_layout(bargap=0.02)
+                st.plotly_chart(fig, use_container_width=True)
+
+                # #teste setores
+                # sector_columns = [col for col in final_df.columns if 'Sector' in col]
+        with tab4:
+            st.subheader("Temperatura ao longo do tempo")
+            col1, col2 = st.columns(2)
+            if 'Track Temp' in df_filtrado.columns:
+                fig_track_temp = px.line(df_filtrado, x='Started at', y='Track Temp', title='Temperatura da Pista')
+                col1.plotly_chart(fig_track_temp, use_container_width=True)
+            if 'Air Temp' in df_filtrado.columns:
+                fig_air_temp = px.line(df_filtrado, x='Started at', y='Air Temp', title='Temperatura do Ar')
+                col2.plotly_chart(fig_air_temp, use_container_width=True)
+                
+        with tab3:
+            if 'Fuel Used' in df_filtrado.columns:
+                st.subheader("Histograma do Consumo de Combustível por Volta")
+                fig_fuel = px.histogram(df_filtrado, x='Fuel Used', nbins=30, title='Consumo por Volta (Fuel Used)')
+                st.plotly_chart(fig_fuel, use_container_width=True)
+
+        # if 'Driver' in df_filtrado.columns and 'Clean' in df_filtrado.columns:
+        #     st.subheader("Voltas Limpas vs Incidentes por Piloto")
+        #     grouped = df_filtrado.groupby(['Driver', 'Clean']).size().reset_index(name='Counts')
+        #     voltas_limpas = grouped[grouped['Clean'] == 1]
+        #     voltas_com_incidentes = grouped[grouped['Clean'] == 0]
+
+        #     fig = go.Figure()
+        #     fig.add_trace(go.Bar(x=voltas_limpas['Driver'], y=voltas_limpas['Counts'], name='Voltas Limpas', marker_color='rgb(25, 128, 37)'))
+        #     fig.add_trace(go.Bar(x=voltas_com_incidentes['Driver'], y=voltas_com_incidentes['Counts'], name='Voltas com Incidente', marker_color='rgb(255, 127, 0)'))
+        #     fig.update_layout(title='Total de Voltas por Piloto', xaxis_title='Piloto', yaxis_title='Número de Voltas', barmode='stack', template='plotly_white')
+        #     st.plotly_chart(fig)
+
+        #     sequencias_df = calcular_sequencias_voltas_limpas(df_filtrado)
+        #     maiores_sequencias = sequencias_df.groupby('Driver')['Sequence_Length'].max().reset_index()
+
+        #     fig_bar = go.Figure(data=[go.Bar(x=maiores_sequencias['Driver'], y=maiores_sequencias['Sequence_Length'], marker_color='rgb(15, 114, 35)')])
+        #     fig_bar.update_layout(title='Maior Sequência de Voltas Limpas por Piloto', xaxis_title='Piloto', yaxis_title='Número de Voltas Limpas Consecutivas', template='plotly_white')
+        #     st.plotly_chart(fig_bar)
+
+        #     for driver in sequencias_df['Driver'].unique():
+        #         driver_data = sequencias_df[sequencias_df['Driver'] == driver]
+        #         fig_hist = go.Figure(data=[go.Histogram(x=driver_data['Sequence_Length'], marker_color='rgb(15, 114, 35)', opacity=0.75)])
+        #         fig_hist.update_layout(title=f'Distribuição das Sequências de Voltas Limpas - {driver}', xaxis_title='Tamanho da Sequência de Voltas Limpas', yaxis_title='Frequência', template='plotly_white')
+        #         st.plotly_chart(fig_hist)
+
+        with tab2:
+
+            try:
+                # Verificar se as colunas necessárias estão presentes
+                if 'Driver' not in final_df.columns or 'Clean' not in final_df.columns:
+                    st.error("O arquivo deve conter as colunas 'Driver' e 'Clean'.")
+                    return
+
+                # Agrupar por piloto e status de volta limpa/incidente
+                
+                grouped = final_df.groupby(['Driver', 'Clean']).size().reset_index(name='Counts')
+
+                # Separar os dados de voltas limpas e com incidentes
+                voltas_limpas = grouped[grouped['Clean'] == 1]
+                voltas_com_incidentes = grouped[grouped['Clean'] == 0]
+
+                # Criar o gráfico de barras empilhadas usando `go.Bar`
+                fig = go.Figure()
+
+                fig.add_trace(go.Bar(
+                    x=voltas_limpas['Driver'],
+                    y=voltas_limpas['Counts'],
+                    name='Voltas Limpas',
+                    marker_color=gear1_colors[0]  # Verde
+                ))
+
+                fig.add_trace(go.Bar(
+                    x=voltas_com_incidentes['Driver'],
+                    y=voltas_com_incidentes['Counts'],
+                    name='Voltas com Incidente',
+                    marker_color=gear1_colors[1]  # laranja
+                ))
+
+                # Configurações do layout
+                fig.update_layout(
+                    title='Total de Voltas por Piloto',
+                    xaxis_title='Piloto',
+                    yaxis_title='Número de Voltas',
+                    barmode='stack',  # Empilhar as barras
+                    template='plotly_white'  # Estilo do gráfico
+                )
+
+                # Exibir o gráfico no Streamlit
+                st.plotly_chart(fig)
+
+            
+
+                # Agrupar por piloto e status de volta limpa/incidente
+                # grouped = final_df.groupby(['Driver', 'Clean']).size().unstack(fill_value=0) #pie
+
+                # # Iterar sobre cada piloto e criar um gráfico de pizza
+                # for driver in grouped.index:
+                #     clean_counts = grouped.loc[driver]
+
+                #     labels = ['Voltas Limpas','Voltas com Incidente']
+                #     values = [clean_counts.get(1, 0), clean_counts.get(0, 0)]
+
+                    
+                #     fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=0.3, pull=[0.1,0], marker_colors=gear1_colors)])
+                #     fig.update_layout(title_text=f'Análise de Voltas Limpas para {driver}')
+
+                #     st.plotly_chart(fig)
+            except UnboundLocalError:
+                st.write("Sem dados a exibir.")
+
+            # Calcular as sequências de voltas limpas
+            sequencias_df = calcular_sequencias_voltas_limpas(final_df)
+
+            # Determinar a maior sequência de voltas limpas por piloto
             maiores_sequencias = sequencias_df.groupby('Driver')['Sequence_Length'].max().reset_index()
 
-            fig_bar = go.Figure(data=[go.Bar(x=maiores_sequencias['Driver'], y=maiores_sequencias['Sequence_Length'], marker_color='rgb(15, 114, 35)')])
-            fig_bar.update_layout(title='Maior Sequência de Voltas Limpas por Piloto', xaxis_title='Piloto', yaxis_title='Número de Voltas Limpas Consecutivas', template='plotly_white')
-            st.plotly_chart(fig_bar)
+            # Criar o gráfico de barras para as maiores sequências
+            fig_bar = go.Figure(data=[
+                go.Bar(
+                    x=maiores_sequencias['Driver'],
+                    y=maiores_sequencias['Sequence_Length'],
+                    marker_color='rgb(15, 114, 35)',  # Verde
+                )
+            ])
 
-            for driver in sequencias_df['Driver'].unique():
-                driver_data = sequencias_df[sequencias_df['Driver'] == driver]
-                fig_hist = go.Figure(data=[go.Histogram(x=driver_data['Sequence_Length'], marker_color='rgb(15, 114, 35)', opacity=0.75)])
-                fig_hist.update_layout(title=f'Distribuição das Sequências de Voltas Limpas - {driver}', xaxis_title='Tamanho da Sequência de Voltas Limpas', yaxis_title='Frequência', template='plotly_white')
-                st.plotly_chart(fig_hist)
+            fig_bar.update_layout(
+                title='Maior Sequência de Voltas Limpas por Piloto',
+                xaxis_title='Piloto',
+                yaxis_title='Número de Voltas Limpas Consecutivas',
+                template='plotly_white'
+            )
+
+            # Exibir o gráfico de barras no Streamlit
+            st.plotly_chart(fig_bar)
 
 if __name__ == "__main__":
     main()
