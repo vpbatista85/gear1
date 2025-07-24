4049239f6b1ca9049e8cfaa1e735f61667132357
Vitor Petta Batista <vitor_pbatista@outlook.com>
2025-07-24 18:22:27 -0300

    adding by car analyses on fuel tab

--- Streamlit/pages/2_Analises.py
+++ Streamlit/pages/2_Analises.py
@@ -1164,7 +1164,6 @@ def main():
                         title="Distribuição Estimada de Voltas por Tanque",
                         labels={"Estimated_Laps": "Voltas por Tanque", "Label": "Piloto" if tipo_analise == "Por Piloto" else "Carro"},
                         color="Label",
-                        boxmean=True
                     )
                     fig.update_layout(xaxis_title="Piloto" if tipo_analise == "Por Piloto" else "Carro", yaxis_title="Voltas Estimadas por Tanque")
                     st.plotly_chart(fig, use_container_width=True)
