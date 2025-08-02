11c6bac0036b23fc3975de61539c791770701277
vpbatista85 <vitor_patista@outlook.com>
2025-08-01 23:01:18 -0300

    updating gear1_app.py

--- Streamlit/pages/get_ip.py
+++ Streamlit/pages/get_ip.py
@@ -1,29 +1,29 @@
-# from streamlit import runtime
-# from streamlit.runtime.scriptrunner import get_script_run_ctx
+# # from streamlit import runtime
+# # from streamlit.runtime.scriptrunner import get_script_run_ctx
 
 
-# def get_remote_ip() -> str:
-#     """Get remote ip."""
+# # def get_remote_ip() -> str:
+# #     """Get remote ip."""
 
-#     try:
-#         ctx = get_script_run_ctx()
-#         if ctx is None:
-#             return None
+# #     try:
+# #         ctx = get_script_run_ctx()
+# #         if ctx is None:
+# #             return None
 
-#         session_info = runtime.get_instance().get_client(ctx.session_id)
-#         if session_info is None:
-#             return None
-#     except Exception as e:
-#         return None
+# #         session_info = runtime.get_instance().get_client(ctx.session_id)
+# #         if session_info is None:
+# #             return None
+# #     except Exception as e:
+# #         return None
 
-#     return session_info.request.remote_ip
+# #     return session_info.request.remote_ip
 
 
+# # import streamlit as st
+
+# # st.title("Title")
+# # st.markdown(f"The remote ip is {get_remote_ip()}")
 # import streamlit as st
 
-# st.title("Title")
-# st.markdown(f"The remote ip is {get_remote_ip()}")
-import streamlit as st
-
-service_account_info = st.secrets["google_service_account"]
-st.write(service_account_info)  # Para verificar se private_key está presente
+# service_account_info = st.secrets["google_service_account"]
+# st.write(service_account_info)  # Para verificar se private_key está presente
